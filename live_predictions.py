d767c6155c70c17c38beb28e816a31237577f0a7
Vinay Arora <vinay.arora7171@hotmail.com>
2020-08-10 15:58:58 +0530

    Get file from CLI

--- live_predictions.py
+++ live_predictions.py
@@ -5,7 +5,9 @@ This file can be used to try a live prediction.
 import keras
 import librosa
 import numpy as np
-
+import argparse
+import sys
+import os
 from config import EXAMPLES_PATH
 from config import MODEL_DIR_PATH
 
@@ -28,18 +30,19 @@ class LivePredictions:
         Method to process the files and create your features.
         """
         data, sampling_rate = librosa.load(self.file)
-        mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
+        mfccs = np.mean(librosa.feature.mfcc(
+            y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
         x = np.expand_dims(mfccs, axis=2)
         x = np.expand_dims(x, axis=0)
         predictions = self.loaded_model.predict_classes(x)
-        print( "Prediction is", " ", self.convert_class_to_emotion(predictions))
+        return self.convert_class_to_emotion(predictions)
 
     @staticmethod
     def convert_class_to_emotion(pred):
         """
         Method to convert the predictions (int) into human readable strings.
         """
-        
+
         label_conversion = {'0': 'neutral',
                             '1': 'calm',
                             '2': 'happy',
@@ -56,8 +59,24 @@ class LivePredictions:
 
 
 if __name__ == '__main__':
-    live_prediction = LivePredictions(file=EXAMPLES_PATH + '03-01-01-01-01-02-05.wav')
+    if len(sys.argv) > 2:
+        print('You have specified too many arguments')
+        sys.exit()
+
+    if len(sys.argv) < 2:
+        print('You need to specify the file path to extract emotions')
+        sys.exit()
+
+    input_path = sys.argv[1]
+    file = EXAMPLES_PATH + input_path
+
+    if not os.path.isfile(file):
+        print('The file specified does not exist', file)
+        sys.exit()
+
+    # live_prediction = LivePredictions(file=EXAMPLES_PATH + '03-01-01-01-01-02-05.wav')
+    live_prediction = LivePredictions(file=EXAMPLES_PATH + input_path)
     live_prediction.loaded_model.summary()
-    live_prediction.make_predictions()
-    live_prediction = LivePredictions(file=EXAMPLES_PATH + '10-16-07-29-82-30-63.wav')
-    live_prediction.make_predictions()
+    print("Prediction is: ", live_prediction.make_predictions())
+    # live_prediction = LivePredictions(file=EXAMPLES_PATH + '10-16-07-29-82-30-63.wav')
+    # live_prediction.make_predictions()
